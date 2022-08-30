fafc07de28fa2fd5928af7d852727338e59bfe58
Leah <LeahNullRCKJL@protonmail.com>
2022-08-30 10:10:45 -0600

    broke squaring math

--- Programming 1/calculate_pi.js
+++ Programming 1/calculate_pi.js
@@ -4,30 +4,29 @@ let Num1 = Math.random()
 //store # of success times and times repeated
 let successes = 0
 let repeated = 0
-//generate random number for the times repeated
+// store random number
 let repeatAttempts = 0
 
-if (repeatAttempts < 5555) {
-    repeatAttempts = Math.floor(Math.random() * 10001)
+//generate a random number between 5000 and 1000000000
+if (repeatAttempts < 5000) {
+    repeatAttempts = Math.floor(Math.random() * 1000000001)
 }
 
 if (repeated < repeatAttempts) {
-    if (Num0^2 + Num1^2 <= 1) {
+    if (Num0** + Num1**) {
         successes++
     }
     repeated++
 }
 let piIsh = 4 * successes / repeated
-console.log("Pi is: " + piIsh, "and this required" + repeated, "attempts.")
+console.log("We calculated pi as: " + piIsh, "and this required " + repeated, " iterations.")
 
 //Create 2 random numbers between 0 and 1 
-
 //If the sum of each number squared (x2 + y2) is less than or equal to 1:
-
 //Success
-
 //Repeat the above many times
-
 //Pi is approximately 4 multiplied by the number of successes divided by the number of times repeated
-
+// [repeated also means iterations]
 //Print the number of repeats (iterations) and the calculated pi to the console.
+
+// Math.pow(5, 2) == 5^2
