5b27f3bdd25120185c2acabbe6ae06b9a2bbc837
Leah <LeahNullRCKJL@protonmail.com>
2022-08-30 19:01:28 -0600

    managed to kinda fix? still outputing 4s though

--- Programming 1/calculate_pi.js
+++ Programming 1/calculate_pi.js
@@ -1,32 +1,32 @@
-// generate 2 random numbers between 0 and 1
-let Num0 = Math.random()
-let Num1 = Math.random()
+// store blank variables for 2 numbers between 0 and 1; an number between 5000000
+let num0 = 0
+let num1 = 0
+let manyTimes = 0
 //store # of success times and times repeated
 let successes = 0
 let repeated = 0
-// store random number
-let repeatAttempts = 0
 
-//generate a random number between 5000 and 1000000000
-if (repeatAttempts < 5000) {
-    repeatAttempts = Math.floor(Math.random() * 1000000001)
+
+//generate a random number between 5,000,000 and 1,000,000,000
+if (manyTimes <= 5000000) {
+    manyTimes = Math.floor(Math.random() * 1000000001)
 }
 
-if (repeated < repeatAttempts) {
-    if (Num0** + Num1**) {
+// todo: add new randomly generated numbers every iteration
+for (Num0 = Math.random(); Num1 = Math.random(); repeated < manyTimes)  {
+    repeated++
+    if (Num0**2 + Num1**2 <= 1) {
         successes++
     }
-    repeated++
 }
 let piIsh = 4 * successes / repeated
 console.log("We calculated pi as: " + piIsh, "and this required " + repeated, " iterations.")
 
-//Create 2 random numbers between 0 and 1 
-//If the sum of each number squared (x2 + y2) is less than or equal to 1:
-//Success
-//Repeat the above many times
-//Pi is approximately 4 multiplied by the number of successes divided by the number of times repeated
-// [repeated also means iterations]
-//Print the number of repeats (iterations) and the calculated pi to the console.
-
-// Math.pow(5, 2) == 5^2
+/*Create 2 random numbers between 0 and 1 
+If the sum of each number squared (x2 + y2) is less than or equal to 1:
+Success
+Repeat the above many times
+Pi is approximately 4 multiplied by the number of successes divided by the number of times repeated
+ [repeated also means iterations]
+Print the number of repeats (iterations) and the calculated pi to the console.
+ Math.pow(5, 2) == 5^2 */
---
+++ Programming 1/text.js
@@ -0,0 +1,2 @@
+let yoot = Math.floor(Math.random() * 1000000000)
+console.log(yoot)
