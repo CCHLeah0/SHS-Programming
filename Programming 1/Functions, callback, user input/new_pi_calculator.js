aa77f796d30c1f6b10ef2373aa6c02174e50af57
CCHLeah0 <LeahNullRCKJL@protonmail.com>
2022-09-26 10:24:46 -0600

    creating the initial file?

---
+++ Programming 1/Functions, callback, user input/new_pi_calculator.js
@@ -0,0 +1,15 @@
+function calculatePi(repeats) {
+    let success = 0
+    for (let i = 0; i < repeats; i++) {
+    // what goes here?
+    num0 = Math.random()
+    num1 = Math.random()
+    if (num0**2 + num1**2 <= 1) {
+        success++
+    }
+}
+
+return (success * 4) / repeats
+}
+
+let pi = calculatePi(1000000)
