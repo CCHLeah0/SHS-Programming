ec8130fa4246457006462a472d72c07709db1674
CCHLeah0 <LeahNullRCKJL@protonmail.com>
2022-09-30 10:18:34 -0600

    comments

--- Programming 1/Functions, callback, user input/project_3.js
+++ Programming 1/Functions, callback, user input/project_3.js
@@ -11,11 +11,21 @@ const userInput = readline.createInterface({
 */
 let finalAns = "Hello, "
 
+// ask user for name and add next part of sentance
+function handleName(answer) {
+  finalAns = finalAns + answer + ". Your favorite color is "
+  userInput.question('What is your favorite color? ', handleColor)
+}
+userInput.question('What is your name? ', handleName)
+
+// ask user favorite color and add consistant next part of sentance
 function handleColor(answer) {
   finalAns = finalAns + answer + " and"
 
   userInput.question("Do you like programming?", handleCode)
 }
+
+// ask user if they like programming, write next part of sentance based on their answer, print it and end
 function handleCode(answer){
   if (answer == "yes") {
     console.log(finalAns, "you like programming.")
@@ -25,15 +35,10 @@ function handleCode(answer){
     console.log(finalAns, "I don't know if you like programming.")
   }
 
-// use this once, at the end of your program
-// calling userInput.question() after this will give an error
-userInput.close()
+  // use this once, at the end of your program
+  // calling userInput.question() after this will give an error
+  userInput.close()
 }
 
 
-function handleName(answer) {
-  finalAns = finalAns + answer + ". Your favorite color is "
-  userInput.question('What is your favorite color? ', handleColor)
-}
-userInput.question('What is your name? ', handleName)
 
