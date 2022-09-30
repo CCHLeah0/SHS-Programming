40925b6b763422b6c032c6341344c8ed011d0bae
CCHLeah0 <LeahNullRCKJL@protonmail.com>
2022-09-30 10:15:13 -0600

    passed my testing

--- Programming 1/Functions, callback, user input/project_3.js
+++ Programming 1/Functions, callback, user input/project_3.js
@@ -9,20 +9,31 @@ const userInput = readline.createInterface({
 /*
   ↑↑↑ Ignore this code (for now) ↑↑↑
 */
+let finalAns = "Hello, "
 
 function handleColor(answer) {
-  console.log(answer)
+  finalAns = finalAns + answer + " and"
 
+  userInput.question("Do you like programming?", handleCode)
+}
+function handleCode(answer){
+  if (answer == "yes") {
+    console.log(finalAns, "you like programming.")
+  } else if (answer == "no") {
+    console.log(finalAns, "you don't like programming.")
+  } else {
+    console.log(finalAns, "I don't know if you like programming.")
+  }
 
+// use this once, at the end of your program
+// calling userInput.question() after this will give an error
+userInput.close()
 }
 
-function handleName(answer) {
-  console.log('Hello ' + answer + ', it is nice to meet you.')
 
+function handleName(answer) {
+  finalAns = finalAns + answer + ". Your favorite color is "
   userInput.question('What is your favorite color? ', handleColor)
 }
 userInput.question('What is your name? ', handleName)
 
-  // use this once, at the end of your program
-  // calling userInput.question() after this will give an error
-  userInput.close()
