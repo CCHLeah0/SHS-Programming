4a458544f02d1346b1359e32eb70e7b135fb4d27
CCHLeah0 <LeahNullRCKJL@protonmail.com>
2022-09-30 09:25:42 -0600

    started project_3

---
+++ Programming 1/Functions, callback, user input/project_3.js
@@ -0,0 +1,11 @@
+/*
+  ↓↓↓ Ignore this code (for now) ↓↓↓
+*/
+const readline = require('readline')
+const userInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+/*
+  ↑↑↑ Ignore this code (for now) ↑↑↑
+*/
