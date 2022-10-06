d3e7b003e361ba39b93bbeb5efbb0951f0213a10
CCHLeah0 <LeahNullRCKJL@protonmail.com>
2022-10-06 09:41:25 -0600

    added notes

--- Programming 1/Async/async.js
+++ Programming 1/Async/async.js
@@ -1,4 +1,15 @@
-
+// a module is code in a different .js file that someone else wrote
+/*
+    Types of code:
+    1 - Code we wrote
+    2- Built in Node/JavaScript
+            Math.random()
+            Math.floor()
+            ect
+    3 - Not built in but written by others (module)
+            import modules by using "require"
+            require('name-of-the-module')
+*/
 let websites = [
     'https://google.com',
     'https://facebook.com',
