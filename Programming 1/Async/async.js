246e3d698d89087fe023da375f38a4da13315556
CCHLeah0 <LeahNullRCKJL@protonmail.com>
2022-10-06 09:38:19 -0600

    started

---
+++ Programming 1/Async/async.js
@@ -0,0 +1,12 @@
+
+let websites = [
+    'https://google.com',
+    'https://facebook.com',
+    'https://youtube.com',
+    'https://amazon.com',
+    'https://instagram.com'
+]
+
+for(let i = 0; i < websites.length; i++) {
+    websites[i]
+}
