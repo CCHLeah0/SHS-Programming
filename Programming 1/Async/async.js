886a8f87881744e31b58fafa1fd8b9edef51997c
CCHLeah0 <LeahNullRCKJL@protonmail.com>
2022-10-10 09:19:59 -0600

    most of template/notes

--- Programming 1/Async/async.js
+++ Programming 1/Async/async.js
@@ -9,15 +9,54 @@
     3 - Not built in but written by others (module)
             import modules by using "require"
             require('name-of-the-module')
+        npm stands for "node package modules"
+        npm is the system to get the modules from the internet
 */
+
+// npm install node-fetch@2
+let fetch = require('node-fetch')
+
 let websites = [
-    'https://google.com',
-    'https://facebook.com',
-    'https://youtube.com',
-    'https://amazon.com',
-    'https://instagram.com'
+    'https://titantheatre.com',
+    'https://shs.davis.k12.ut.us',
+    'https://syracusehigh.org',
+    'https://kahoot.it',
+    'https://dsd.instructure.com'
 ]
 
+/*
+    promise - data/process that isn't complete
+    Promises run asynchronously
+    fetch gives a promise
+    promise.then() uses a callback to run code when the promise is done
+    for loops are synchronous
+*/
+
 for(let i = 0; i < websites.length; i++) {
-    websites[i]
+    console.log('start '+ websites[i])
+    let promise = fetch(websites[i])
+        
+    function callback() {
+        console.log('done '+ websites[i])
+    }
+
+    promise.then(callback)
+}
+
+console.log('here')
+
+/*
+"await" waits on a promise to finish before moving on
+await can only be used in async functions
+use the word "async" before "function" to make it async
+this is how we turn something asynch into something synchronous
+*/
+async function fetchWebsite(i) {
+    await fetch(websites[i])
+}
+
+for (let i = 0; i < websites.length; i++) {
+    console.log('async/await start '+websites[i])
+    fetchWebsite(i)
+    console.log('async/await done ' + websites[i])
 }
