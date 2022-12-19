3d04eca610ca485b7d39a4062ba8c1c2d0f5c39c
CCHLeah0 <LeahNullRCKJL@protonmail.com>
2022-12-19 08:46:27 -0700

    I broke it, it burned my hand so i punched it

---
+++ Programming 1/term 2/Project 6/Blackjack.js
@@ -0,0 +1,11 @@
+const cards = [Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King]; // attempt at making array for cards
+// 0 == Ace
+// 1-9 is 2-10
+// 10 == Jack, 11 == Queen, 12 == King
+
+// dealer?
+let dealer =
+
+const hit =(dealer, player) => {
+
+}
