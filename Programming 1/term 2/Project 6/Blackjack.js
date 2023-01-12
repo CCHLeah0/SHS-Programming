cb76efbcf82d2feba5888417b8ce3d4fda8fdb67
CCHLeah0 <LeahNullRCKJL@protonmail.com>
2023-01-11 20:52:21 -0700

    deck generator and shuffler

--- Programming 1/term 2/Project 6/Blackjack.js
+++ Programming 1/term 2/Project 6/Blackjack.js
@@ -1,11 +1,66 @@
-const cards = [Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King]; // attempt at making array for cards
-// 0 == Ace
-// 1-9 is 2-10
-// 10 == Jack, 11 == Queen, 12 == King
+// <found online>
+// program to shuffle the deck of cards 
+// declare card elements
+const suits = ["Spades", "Diamonds", "Club", "Heart"];
+const values = [
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+]; // values
+
+// empty array to contain cards
+let deck = [];
+
+// create a deck of cards
+for (let i = 0; i < suits.length; i++) {
+    for (let x = 0; x < values.length; x++) {
+        let card = { Value: values[x], Suit: suits[i] };
+        deck.push(card);
+    }
+}
+
+// shuffle the cards
+for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+}
+
+console.log('The first five cards are:');
+
+// display 5 results
+for (let i = 0; i < 5; i++) {
+    console.log(`${deck[i].Value} of ${deck[i].Suit}`)
+}
+
+// random numbers
+function randomIntFromInterval(min, max) { // min and max included 
+    // always input 
+    return Math.floor(Math.random() * (max - min) + min)
+} //randomIntFromInterval
+// </found online>
+
+const rndInt = randomIntFromInterval(0, 51)
+console.log(rndInt)
 
 // dealer?
-let dealer =
+let dealer = new Object()
 
-const hit =(dealer, player) => {
+let player = new Object(hand)
 
-}
+function hit(dealer, player) {
+    if (playerHit = True) {
+        //give player a random card from remaining deck
+    }
+} // Hit
---
+++ Programming 1/term 2/Project 6/OldAttempts.js
@@ -0,0 +1,32 @@
+// Old code
+/* const cards = [Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King]; */ // attempt at making array for cards
+// 0 == Ace
+// 1-9 is 2-10
+// 10 == Jack, 11 == Queen, 12 == King
+
+// hearts, spades, clubs, diamonds
+const deck = ["Ace of Spades", "Ace of Diamonds", "Ace of Hearts", "Ace of Clubs", "Two of Spades", "Two of Diamonds", "Two of Hearts", "Two of Clubs", "Three of Spades", "Three of Diamonds", "Three of Hearts", "Three of Clubs", "Four of Spades", "Four of Diamonds", "Four of Hearts", "Four of Clubs", "Five of Spades", "Five of Diamonds", "Five of Hearts", "Five of Clubs", "Six of Spades", "Six of Diamonds", "Six of Hearts", "Six of Clubs", "Seven of Spades", "Seven of Diamonds", "Seven of Hearts", "Seven of Clubs", "Eight of Spades", "Eight of Diamonds", "Eight of Hearts", "Eight of Clubs", "Nine of Spades", "Nine of Diamonds", "Nine of Hearts", "Nine of Clubs", "Ten of Spades", "Ten of Diamonds", "Ten of Hearts", "Ten of Clubs", "Jack of Spades", "Jack of Diamonds", "Jack of Hearts", "Jack of Clubs", "Queen of Spades", "Queen of Diamonds", "Queen of Hearts", "Queen of Clubs", "King of Spades", "King of Diamonds", "King of Hearts", "King of Clubs"]
+/*
+0-3 Ace
+4-7 Two
+8-11 Three
+12-15 Four
+16-19 Five
+20-23 Six
+24-27 Seven
+28-31 Eight
+32-35 Nine
+36-39 Ten
+40-43 Jack
+44-47 Queen
+48-51 King
+
+Spades:
+0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48
+Diamonds:
+1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49
+Hearts:
+2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50
+Clubs:
+3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51
+*/
